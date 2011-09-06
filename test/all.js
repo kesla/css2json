cfb696356b151ced8c1369c35db1c809f9a2518c
David Björklund <davidbj@kth.se>
2011-09-06 19:10:08 +0200

    Move fixtures to own directory

--- test/all.js
+++ test/all.js
@@ -7,7 +7,7 @@ var assert = require('assert'),
 var bases = ['simple', 'advanced', 'advanced2'];
 
 bases.forEach(function(base){
-  var basePath = path.join(__dirname, base);
+  var basePath = path.join(__dirname, "fixtures", base);
   var css = fs.readFileSync(basePath + '.css', 'utf8');
      json = JSON2.parse(fs.readFileSync(basePath + '.json', 'utf8'));
 
