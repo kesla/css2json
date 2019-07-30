4d405e855865436528667cbc335e63fb09ad3af8
David Björklund <david.bjorklund@gmail.com>
2019-07-30 17:17:41 +0200

    get "yarn test" to work

--- test/all.js
+++ test/all.js
@@ -1,7 +1,6 @@
 var assert = require('assert'),
     css2json = require('../lib/css2json'),
     fs = require('fs'),
-    JSON2 = require("json-san"),
     path = require('path');
 
 var bases = ['simple', 'advanced', 'advanced2', 'comments'];
@@ -9,7 +8,7 @@ var bases = ['simple', 'advanced', 'advanced2', 'comments'];
 bases.forEach(function(base){
   var basePath = path.join(__dirname, "fixtures", base);
   var css = fs.readFileSync(basePath + '.css', 'utf8'),
-     json = JSON2.parse(fs.readFileSync(basePath + '.json', 'utf8'));
+     json = JSON.parse(fs.readFileSync(basePath + '.json', 'utf8'));
 
   assert.deepEqual(css2json(css), json);
 });
